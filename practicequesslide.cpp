779a0cfc652612ee75f57d80245e5bda4fd6de5c
Amandeep Singh <121692605+iaman29@users.noreply.github.com>
2023-03-22 00:59:08 +0530

    Update practicequesslide.cpp

--- practicequesslide.cpp
+++ practicequesslide.cpp
@@ -1,13 +1,44 @@
 #include<iostream>
 using namespace std;
 class vehicle
-{public:
+{
+    public:
     int wheels;
     string name;
     string type;
-    void drive(int,string,string);
+    
+
+};
+class two:public vehicle{
+    public:
+    void drive(int a,string b,string c){
+        cout<<"number of wheels"<<wheels<<endl<<"name"<<name<<"type"<<type<<endl;
+        wheels=a;
+        name=b;
+        type=c;}
+        void drive(int a,int b){
+            wheels=a;
+        name=b;
+        cout<<"number of wheels"<<wheels<<endl<<"name"<<name<<endl;
+        }
+
+};
+class four:public vehicle{
+    public:
+    void drive(int a,string b,string c){
+        wheels=a;
+        name=b;
+        type=c;
+        cout<<"number of wheels"<<wheels<<endl<<"name"<<name<<"type"<<type<<endl; }
+        void drive(int a,int b){
+            wheels=a;
+        name=b;
+        cout<<"number of wheels"<<wheels<<endl<<"name"<<name<<endl;
+        }
 };
 
 int main (){
+    four f1;
+    f1.drive(4,"ferrari");
 
 }
