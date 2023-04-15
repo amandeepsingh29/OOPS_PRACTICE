ca9dbddcb5fe40db686f61db3645e0fcea39064a
Amandeep Singh <121692605+iaman29@users.noreply.github.com>
2023-04-15 22:06:47 +0530

    code done

--- ass5_q1.cpp
+++ ass5_q1.cpp
@@ -1,9 +1,12 @@
 #include <iostream> 
  using namespace std; 
-  class loc {
-int longitude, latitude;  public:
-loc() {}
-loc(int lg, int lt)  longitude = lg;  latitude = lt;
+class loc {
+int longitude, latitude;
+public:
+  loc() {}
+  loc(int lg, int lt) {
+     longitude = lg;  
+     latitude = lt;
 }
 void show() {
 cout << longitude << " ";  cout << latitude << "\n";
@@ -19,7 +22,8 @@ cout << "In overloaded new.\n";
 p = malloc(size);
 return p;
 }
-// delete overloaded relative to loc  void loc::operator delete(void *p)
+// delete overloaded relative to loc  
+void loc::operator delete(void *p)
 {
 cout << "In overloaded delete.\n";  free(p);
 }
@@ -29,7 +33,10 @@ loc *p1, *p2;
 
 p1 = new loc (10, 20);
 p2 = new loc (-10, -20);
-}
+
 p1->show();
-p2->show();  delete p1;  delete p2;  return 0;
+p2->show();  
+delete p1;  
+delete p2;  
+return 0;
 }
