63cbdca7d8a323c18868241abdf17d09aaaf0e39
Amandeep Singh <amandeep29032004@gmail.com>
2023-04-30 21:49:39 +0530

    overload arrow->

---
+++ overload_arrow.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class myclass{
+    public:
+    int i;
+    myclass *operator->(){
+        return this;
+    }
+} ;
+
+int main(){
+    myclass ob;
+    ob->i=10;
+    cout << ob.i << " " << endl<< ob->i;
+
+}
