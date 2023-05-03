3ff015f5dd935f6e78071dfc5f4b5debd82fda85
Amandeep Singh <amandeep29032004@gmail.com>
2023-05-03 16:34:04 +0530

    vowels counter

--- File_handling/start.cpp
+++ File_handling/start.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
-class myclass{
-    
-};
+
+int main() {
+    int count = 0;
+    char c;
+    ifstream fin("MyFile.txt");
+    if (!fin) {
+        cout << "File Does not Exist";
+        return 0;
+    }
+    while (fin.get(c)) {
+        cout << c;
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            count++;
+        }
+    }
+    fin.close();
+    cout << endl << "count is " << count;
+    return 0;
+}
